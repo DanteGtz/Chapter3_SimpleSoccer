9ed19fab61be413efe215c76d6f1adbf59db19f5
DanteGtz <neo31117@gmail.com>
2017-05-19 17:51:54 -0500

    The declaration of some member methods within BGE

--- Chapter3_SimpleSoccer/BaseGameEntity.cpp
+++ Chapter3_SimpleSoccer/BaseGameEntity.cpp
@@ -5,26 +5,27 @@
 
 int BaseGameEntity::m_iNextValidID = 0;
 
-//------------------------------ ctor -----------------------------------------
-//-----------------------------------------------------------------------------
+//-----------------> Constructor <----------------//
+
 BaseGameEntity::BaseGameEntity(int ID) :m_dBoundingRadius(0.0),
-m_vScale(Vector2D(1.0, 1.0)),
-m_iType(default_entity_type),
-m_bTag(false)
+m_vScale(Vector2D(1.0, 1.0)), m_iType(DEFAULT_ENTITY_TYPE), m_bTag(false)
 {
-	SetID(ID);
+	setID(ID);
 }
 
-//----------------------------- SetID -----------------------------------------
-//
-//  this must be called within each constructor to make sure the ID is set
-//  correctly. It verifies that the value passed to the method is greater
-//  or equal to the next valid ID, before setting the ID and incrementing
-//  the next valid ID
-//-----------------------------------------------------------------------------
-void BaseGameEntity::SetID(int val)
+
+
+//-----------------> set identificador <----------------//
+
+//Este debe ser llamado dentro de cada constructor para asegurarnos
+//que el ID se ha asignado correctamente. Nos aseguramos que el
+//valor pasado sea igual o mayor al siguiente indentificador
+//valido, antes asignarselo al ID e incrementar el proximo
+//identificador valido
+void BaseGameEntity::setID(int val)
 {
-	//make sure the val is equal to or greater than the next available ID
+	//Nos aseguramos que el valor es igual o mayor que el siguient
+	//identificador disponible
 	assert((val >= m_iNextValidID) && "<BaseGameEntity::SetID>: invalid ID");
 
 	m_ID = val;
