37487c86d95e71153693c0248721f9b78ea5395e
DanteGtz <neo31117@gmail.com>
2017-05-19 14:37:14 -0500

    Analytic geometry for collision between entities
    Adds the methods to detect collision between walls and Entities,
    Entities between Entities

--- Chapter3_SimpleSoccer/geometry.h
+++ Chapter3_SimpleSoccer/geometry.h
@@ -1,4 +1,5 @@
 #pragma once
+//Analytic geometry for collision between entities
 #include <math.h>
 #include <vector>
 
@@ -49,7 +50,7 @@ inline double distToLineSegment(Vector2D A,
 //Dado un segmanto de linea AB y el punto P, esta funcion
 //calcula la distancia perpendicular entre ellos. Omite el paso
 //de sacar raiz cuadrada
-inline double distToLineSegment(Vector2D A,
+inline double distToLineSegmentSq(Vector2D A,
 	Vector2D B, Vector2D P)
 {
 	//Si el angulo entre PA y AB es obtuso, entonces el vertice mas
@@ -116,6 +117,64 @@ inline bool doRayCircleIntersect(Vector2D rayOrigin,
 }
 
 
+//-----------------> El punto de interseccion mas cercano entre <----------------//
+//-----------------> un segmento de linea y una circunferencia <----------------//
+
+//Dado un segmento de linea AB, la ubicacion de una circunferencia y su radio
+//esta funcion determina si hay una interseccion y almacena la posicion de la
+//interseccion mas cercana en una refecencia llamada
+//intersectionPoint.
+//Regresa falso si no se encontro un punto de interseccion
+inline bool getLineSegmentCircleClosestIntersectionPoint(Vector2D A, Vector2D B,
+	Vector2D pos, double    radius, Vector2D& intersectionPoint)
+{
+	Vector2D toBNorm = vec2DNormalize(B - A);
+	Vector2D perp = toBNorm.perp();
+
+	//Translada al circulo dentro de un espacio local definido por le vector
+	//B-A con origen en punto A'
+	//  Vector2D LocalPos = PointToLocalSpace(pos, toBNorm, toBNorm.Perp(), A);
+	Vector2D localPos = pointToLocalSpace(pos, toBNorm, perp, A);
+
+	bool ipFound = false;
+
+	//Si a la suma del vector de la posicion local y el del radio resulta ser
+	//negativo, entonce la circunferencia se encuentra atras del punto A, por
+	//lo tanto niguna interseccion es posible. Por otro lado si la variable
+	//localPos.x menos el radio es mayor a la longitud del segmento AB,
+	//entonces ninguna interseccion es posible
+	if ((localPos.x + radius >= 0) &&
+		((localPos.x - radius)*(localPos.x - radius) <= vector2DDistanceSq(B, A)))
+	{
+		//Si la distancia desde el eje x a la posicion del objeto es menor
+		//que el radio entonces hay una posible interseccion
+		if (fabs(localPos.y) < radius)
+		{
+			//Ahora hagamos la interseccion entre la circunferencia y
+			//el segmento. El centro del circulo esta representado por C(A,B)
+			//Este punto de interseccion esta dado por la formula
+			//x = A +/-sqrt(r^2-B^2), y=0. Solo cuando necesitamos dar con el
+			//valor mas chico positivo de x
+			double a = localPos.x;
+			double b = localPos.y;
+
+			double ip = a - sqrt(radius*radius - b*b);
+
+			if (ip <= 0)
+			{
+				ip = a + sqrt(radius*radius - b*b);
+			}
+
+			ipFound = true;
+
+			intersectionPoint = A + toBNorm*ip;
+		}
+	}
+
+	return ipFound;
+}
+
+
 
 //-----------------> Interseccion entre un segmento de linea <----------------//
 //-----------------> (en su forma vectorial) y una circunferencia <----------------//
@@ -145,7 +204,7 @@ inline double getRayCircleIntersect(Vector2D rayOrigin,
 //la circunferencia que intersecta con las tangentes definidas
 //por el punto P y uno de los dos puntos de la circunferencia
 //Regresa falso si P esta dentro del circulo
-inline bool GetTangentPoints(Vector2D C, double R, Vector2D P, 
+inline bool getTangentPoints(Vector2D C, double R, Vector2D P, 
 	Vector2D& T1, Vector2D& T2)
 {
 	Vector2D PmC = P - C;
@@ -212,7 +271,7 @@ inline bool lineIntersection2D(Vector2D A,
 //regresa verdadero si que existe una interseccion. Si
 //esto ocurre devuelve la distancia a la que ocurre la
 //interseccion
-inline bool LineIntersection2D(Vector2D A, Vector2D B,
+inline bool lineIntersection2D(Vector2D A, Vector2D B,
 	Vector2D C, Vector2D D, double &dist)
 {
 
@@ -259,7 +318,7 @@ inline bool LineIntersection2D(Vector2D A, Vector2D B,
 //esto ocurre devuelve la distancia a la que ocurre la
 //interseccion. Ademas regresa un vector dirigido al
 //punto de interseccion
-inline bool LineIntersection2D(Vector2D   A, Vector2D   B,
+inline bool lineIntersection2D(Vector2D   A, Vector2D   B,
 	Vector2D   C, Vector2D   D, double&     dist, Vector2D&  point)
 {
 
@@ -293,4 +352,97 @@ inline bool LineIntersection2D(Vector2D   A, Vector2D   B,
 
 		return false;
 	}
+}
+
+
+
+//-----------------> Interseccion de un segmento <----------------//
+//-----------------> de recta a una circunferencia <----------------//
+
+//Regresa cierto si el segmento de linea AB in
+inline bool   lineSegmentCircleIntersection(Vector2D A,
+	Vector2D B, Vector2D P, double    radius)
+{
+	//Primero determina la distancia desde el centro del circulo
+	//al segmento de linea
+	double distToLineSq = distToLineSegmentSq(A, B, P);
+
+	if (distToLineSq < radius*radius)
+	{
+		return true;
+	}
+
+	else
+	{
+		return false;
+	}
+
+}
+
+
+
+//-----------------> Punto en la circunferencia <----------------//
+
+//Regresa verdadero si el punto P esta dentro del radio de 
+//cierta circunferencia dada
+inline bool pointInCircle(Vector2D pos, double    radius,
+	Vector2D p)
+{
+	double distFromCenterSquared = (p - pos).lengthSq();
+
+	if (distFromCenterSquared < (radius*radius))
+	{
+		return true;
+	}
+
+	return false;
+}
+
+
+//-----------------> Interseccion de objetos 2D <----------------//
+
+//Prueba si dos poligonos que se intersectan
+//*No revisa si el poligono es cerrado*
+inline bool objectIntersection2D(const std::vector<Vector2D>& object1,
+	const std::vector<Vector2D>& object2)
+{
+	//Prueba cada segmento de linea del primer poligono contra
+	//cada segmento del poligono
+	for (unsigned int r = 0; r < object1.size() - 1; ++r)
+	{
+		for (unsigned int t = 0; t < object2.size() - 1; ++t)
+		{
+			if (lineIntersection2D(object2[t],
+				object2[t + 1],
+				object1[r],
+				object1[r + 1]))
+			{
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+
+//-----------------> Interseccion de un segmento <----------------//
+//-----------------> y un objeto en 2D <----------------//
+
+//Prueba la interseccion entre el segmento de linea
+//y el poligono de colision *No revisa si el poligono es cerrado*
+inline bool segmentObjectIntersection2D(const Vector2D& A,
+	const Vector2D& B, const std::vector<Vector2D>& object)
+{
+	//Prueba el segmento AB contra cada segmento
+	//del poligono
+	for (unsigned int r = 0; r<object.size() - 1; ++r)
+	{
+		if (lineIntersection2D(A, B, object[r], object[r + 1]))
+		{
+			return true;
+		}
+	}
+
+	return false;
 }
