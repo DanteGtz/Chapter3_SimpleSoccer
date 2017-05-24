6ce09951f8ec29df11b47f2accf5e19618c92bfb
DanteGtz <neo31117@gmail.com>
2017-05-24 13:22:01 -0500

    various methods that operate on or with streams

--- Chapter3_SimpleSoccer/Stream_Utility_Funstions.h
+++ Chapter3_SimpleSoccer/Stream_Utility_Funstions.h
@@ -1,23 +1,14 @@
 #pragma once
 #ifndef STREAM_UTILITY_FUNCTIONS
 #define STREAM_UTILITY_FUNCTIONS
-//-----------------------------------------------------------------------------
-//
-//  Name:   Stream_Utility_Functions.h
-//
-//  Author: Mat Buckland (www.ai-junkie.com)
-//
-//  Desc:   various useful functions that operate on or with streams
-//-----------------------------------------------------------------------------
+//various methods that operate on or with streams
 #include <sstream>
 #include <string>
 #include <iomanip>
 
+//-----------------> ttos <----------------//
 
-//------------------------------ ttos -----------------------------------------
-//
-//  convert a type to a string
-//-----------------------------------------------------------------------------
+//Convierte de un tipo de datos (porque es un template) a un cadena
 template <class T>
 inline std::string ttos(const T& t, int precision = 2)
 {
@@ -28,20 +19,23 @@ inline std::string ttos(const T& t, int precision = 2)
 	return buffer.str();
 }
 
-//------------------------------ ttos -----------------------------------------
-//
-//  convert a bool to a string
-//-----------------------------------------------------------------------------
+
+
+
+//-----------------> btos <----------------//
+
+//Convierte de un boleano a un cadena
 inline std::string btos(bool b)
 {
 	if (b) return "true";
 	return "false";
 }
 
-//--------------------------- GetValueFromStream ------------------------------
-//
-//  grabs a value of the specified type from an input stream
-//-----------------------------------------------------------------------------
+
+
+//-----------------> Obten un valor del stream <----------------//
+
+//Agarra un valor del tipo especificado del stream de entrada
 template <typename T>
 inline T GetValueFromStream(std::ifstream& stream)
 {
@@ -49,7 +43,7 @@ inline T GetValueFromStream(std::ifstream& stream)
 
 	stream >> val;
 
-	//make sure it was the correct type
+	//Se asegura que sea del tipo de datos correcto
 	if (!stream)
 	{
 		throw std::runtime_error("Attempting to retrieve wrong type from stream");
