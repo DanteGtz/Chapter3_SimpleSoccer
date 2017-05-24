b72a824d0b24081011819a1579a1c8ea44313434
DanteGtz <neo31117@gmail.com>
2017-05-24 13:08:49 -0500

    The wall2D class

--- Chapter3_SimpleSoccer/Wall2D.h
+++ Chapter3_SimpleSoccer/Wall2D.h
@@ -2,19 +2,9 @@
 #ifndef WALL_H
 #define WALL_H
 //The wall2D class
-//------------------------------------------------------------------------
-//
-//  Name:   Wall2D.h
-//
-//  Desc:   class to create and render 2D walls. Defined as the two 
-//          vectors A - B with a perpendicular normal. 
-//          
-//
-//  Author: Mat Buckland (fup@ai-junkie.com)
-//
-//------------------------------------------------------------------------
-#include "../misc/Cgdi.h"
-#include "../2D/Vector2D.h"
+
+#include "Cgdi.h"
+#include "Vector2D.h"
 #include <fstream>
 
 
@@ -22,13 +12,13 @@ class Wall2D
 {
 protected:
 
-	Vector2D    m_vA,
-		m_vB,
-		m_vN;
+	Vector2D    m_vA;
+	Vector2D	m_vB;
+	Vector2D	m_vN;
 
 	void CalculateNormal()
 	{
-		Vector2D temp = Vec2DNormalize(m_vB - m_vA);
+		Vector2D temp = vec2DNormalize(m_vB - m_vA);
 
 		m_vN.x = -temp.y;
 		m_vN.y = temp.x;
@@ -38,19 +28,21 @@ public:
 
 	Wall2D() {}
 
-	Wall2D(Vector2D A, Vector2D B) :m_vA(A), m_vB(B)
+	Wall2D(Vector2D A, Vector2D B) 
+		:m_vA(A), m_vB(B)
 	{
 		CalculateNormal();
 	}
 
-	Wall2D(Vector2D A, Vector2D B, Vector2D N) :m_vA(A), m_vB(B), m_vN(N)
+	Wall2D(Vector2D A, Vector2D B, Vector2D N)
+		:m_vA(A), m_vB(B), m_vN(N)
 	{ }
 
-	Wall2D(std::ifstream& in) { Read(in); }
+	Wall2D(std::ifstream& in) { read(in); }
 
 	virtual void Render(bool RenderNormals = false)const
 	{
-		gdi->Line(m_vA, m_vB);
+		gdi->line(m_vA, m_vB);
 
 		//render the normals if rqd
 		if (RenderNormals)
@@ -58,42 +50,42 @@ public:
 			int MidX = (int)((m_vA.x + m_vB.x) / 2);
 			int MidY = (int)((m_vA.y + m_vB.y) / 2);
 
-			gdi->Line(MidX, MidY, (int)(MidX + (m_vN.x * 5)), (int)(MidY + (m_vN.y * 5)));
+			gdi->line(MidX, MidY, (int)(MidX + (m_vN.x * 5)), (int)(MidY + (m_vN.y * 5)));
 		}
 	}
 
-	Vector2D From()const { return m_vA; }
-	void     SetFrom(Vector2D v) { m_vA = v; CalculateNormal(); }
+	Vector2D from()const { return m_vA; }
+	void     setFrom(Vector2D v) { m_vA = v; CalculateNormal(); }
 
-	Vector2D To()const { return m_vB; }
-	void     SetTo(Vector2D v) { m_vB = v; CalculateNormal(); }
+	Vector2D to()const { return m_vB; }
+	void     setTo(Vector2D v) { m_vB = v; CalculateNormal(); }
 
-	Vector2D Normal()const { return m_vN; }
-	void     SetNormal(Vector2D n) { m_vN = n; }
+	Vector2D normal()const { return m_vN; }
+	void     setNormal(Vector2D n) { m_vN = n; }
 
-	Vector2D Center()const { return (m_vA + m_vB) / 2.0; }
+	Vector2D center()const { return (m_vA + m_vB) / 2.0; }
 
-	std::ostream& Wall2D::Write(std::ostream& os)const
+	std::ostream& Wall2D::write(std::ostream& os)const
 	{
 		os << std::endl;
-		os << From() << ",";
-		os << To() << ",";
-		os << Normal();
+		os << from() << ",";
+		os << to() << ",";
+		os << normal();
 		return os;
 	}
 
-	void Read(std::ifstream& in)
+	void read(std::ifstream& in)
 	{
 		double x, y;
 
 		in >> x >> y;
-		SetFrom(Vector2D(x, y));
+		setFrom(Vector2D(x, y));
 
 		in >> x >> y;
-		SetTo(Vector2D(x, y));
+		setTo(Vector2D(x, y));
 
 		in >> x >> y;
-		SetNormal(Vector2D(x, y));
+		setNormal(Vector2D(x, y));
 	}
 
 };
