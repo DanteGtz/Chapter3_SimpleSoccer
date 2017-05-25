032e3c83f65e50287d9b9472f45c6a406dcef78b
DanteGtz <neo31117@gmail.com>
2017-05-25 11:44:33 -0500

    //An event controller for the console application

--- Chapter3_SimpleSoccer/WindowsUtils.h
+++ Chapter3_SimpleSoccer/WindowsUtils.h
@@ -10,74 +10,77 @@
 
 struct Vector2D;
 
-//macro to detect keypresses
+//Variable macro que detecta si una tecla es presionada
 #define KEYDOWN(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 1 : 0)
 
 #define WAS_KEY_PRESSED(vk_code) ((GetKeyState(vk_code) & 0x8000) != 0)
 #define IS_KEY_PRESSED(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) != 0)
 
-//Call this to refresh the client window
-inline void RedrawWindow(HWND hwnd, bool RedrawBackGround = true)
+//Esta funcion se llama para refrescar la pantalla
+inline void redrawWindow(HWND hwnd, bool redrawBackGround = true)
 {
-	InvalidateRect(hwnd, NULL, RedrawBackGround);
+	InvalidateRect(hwnd, NULL, redrawBackGround);
 	UpdateWindow(hwnd);
 }
 
-//Call this to refresh the client window
-inline void RedrawWindowRect(HWND hwnd, bool RedrawBackGround, RECT& RedrawArea)
+//Se llama cada vez que haya que refrescar la pantalla del cliente
+inline void RedrawWindowRect(HWND hwnd, bool redrawBackGround, RECT& redrawArea)
 {
-	InvalidateRect(hwnd, &RedrawArea, RedrawBackGround);
+	InvalidateRect(hwnd, &redrawArea, redrawBackGround);
 	UpdateWindow(hwnd);
 }
 
+//Cambia los estados de item menu dados un identificador item,
+//el estado deseado y el HWND(pantalla) del menu del usuario
+void changeMenuState(HWND hwnd, UINT menuItem, UINT state);
 
-//Changes the state of a menu item given the item identifier, the 
-//desired state and the HWND of the menu owner
-void ChangeMenuState(HWND hwnd, UINT MenuItem, UINT state);
+//Si b es verdaderdo, la variable menuItem se verifica
+//se deja sin verificar
+void checkMenuItemAppropriately(HWND hwnd, UINT menuItem, bool b);
 
-//if b is true MenuItem is checked, otherwise it is unchecked
-void CheckMenuItemAppropriately(HWND hwnd, UINT MenuItem, bool b);
+//Esta funcion es un remplazo del metodo StringCchLength que encontramos
+//en el SDK de la plataforma windows. Puedes visitar MSDN para mas
+//detalles. Solo se utiliza para checar las cadenas de la barra de 
+//herramientas
+bool checkBufferLength(char* buff, int MaxLength, unsigned int& BufferLength);
+
+void errorBox(std::string& msg);
+void errorBox(char* msg);
+
+//Obtiene las coordenadas del cursor relativo a la ventana
+//activa
+Vector2D getClientCursorPosition();
+
+//Obtiene las coordenadas del cursor relativo a la ventana
+//activa 
+Vector2D getClientCursorPosition(HWND hwnd);
 
 
-//this is a replacement for the StringCchLength function found in the 
-//platform SDK. See MSDN for details. Only ever used for checking toolbar
-//strings
-bool CheckBufferLength(char* buff, int MaxLength, unsigned int& BufferLength);
-
-void ErrorBox(std::string& msg);
-void ErrorBox(char* msg);
-
-//gets the coordinates of the cursor relative to an active window 
-Vector2D GetClientCursorPosition();
-
-//gets the coordinates of the cursor relative to an active window 
-Vector2D GetClientCursorPosition(HWND hwnd);
-
-
-//two handy functions from Mr Petzold. They open a common dialog box to
-//grab a filename
-void FileInitialize(HWND hwnd,
+//Estos metodos abren el common dialog box para obtener un 
+//nombre de archivo
+void fileInitialize(HWND hwnd,
 	OPENFILENAME& ofn,
 	const std::string& defaultFileTypeDescription,
 	const std::string& defaultFileExtension);
 
-BOOL FileOpenDlg(HWND               hwnd,
+BOOL fileOpenDlg(HWND               hwnd,
 	PTSTR              pstrFileName,
 	PTSTR              pstrTitleName,
 	const std::string& defaultFileTypeDescription,
 	const std::string& defaultFileExtension);
 
-BOOL FileSaveDlg(HWND hwnd,
+BOOL fileSaveDlg(HWND hwnd,
 	PTSTR pstrFileName,
 	PTSTR pstrTitleName,
 	const std::string& defaultFileTypeDescription,
 	const std::string& defaultFileExtension);
 
-//call this to resize the specified window to the specified size.
-void ResizeWindow(HWND hwnd, int cx, int cy);
+//Funcion que al invocarse cambia las medidas de la pantalla
+//especificas por los argumentos
+void resizeWindow(HWND hwnd, int cx, int cy);
 
-int  GetWindowHeight(HWND hwnd);
-int  GetWindowWidth(HWND hwnd);
+int  getWindowHeight(HWND hwnd);
+int  getWindowWidth(HWND hwnd);
 
 
 
