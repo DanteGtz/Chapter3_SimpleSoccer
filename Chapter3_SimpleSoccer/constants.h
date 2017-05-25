f94d51a463e19533b9b66a121020a88ff542eed2
DanteGtz <neo31117@gmail.com>
2017-05-25 11:22:10 -0500

    Three simple constants
    A label has been modified

--- Chapter3_SimpleSoccer/constants.h
+++ Chapter3_SimpleSoccer/constants.h
@@ -5,7 +5,8 @@
 const int WindowWidth = 700;
 const int WindowHeight = 400;
 
-							//Define el tamanho del equipo --favor de cambiar
+							//Define el numero de jugadores
+							//por equipo --favor de no cambiar
 const int TeamSize = 5;
 
 
